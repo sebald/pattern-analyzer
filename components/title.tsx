f8894ee5a02745f718b195d16a3ed88e22c859f2
Sebastian Sebald <sebastian.sebald@gmail.com>
2023-01-11 21:20:16 +0100

    title uses primary color

--- components/title.tsx
+++ components/title.tsx
@@ -3,5 +3,7 @@ export interface TitleProps {
 }
 
 export const Title = ({ children }: TitleProps) => (
-  <h1 className="text-4xl font-bold tracking-tight">{children}</h1>
+  <h1 className="text-4xl font-bold tracking-tight text-primary-700">
+    {children}
+  </h1>
 );
