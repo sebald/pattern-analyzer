694457e643fc643b29b3483d061d2a8fc44fc227
Sebastian Sebald <sebastian.sebald@reservix.de>
2023-01-15 23:25:01 +0100

    refa button and install cva

--- components/caption.tsx
+++ components/caption.tsx
@@ -1,7 +1,11 @@
+// Props
+// ---------------
 export interface CaptionProps {
   children: React.ReactNode;
 }
 
+// Components
+// ---------------
 export const Caption = ({ children }: CaptionProps) => (
   <h6 className="text-sm text-primary-300">{children}</h6>
 );
