5125727ff757ecb67ee824fe360f4999b73cf6c9
Sebastian Sebald <sebastian.sebald@gmail.com>
2023-01-17 13:08:55 +0100

    feat: use `cheerio` for crawling and add event name

--- app/[event]/page.tsx
+++ app/[event]/page.tsx
@@ -1,5 +1,7 @@
 import { Caption, Center, Message, Title } from 'components';
+import { getEvent } from 'lib/longshanks';
 import type { XWSSquad } from 'lib/xws';
+
 import { Filter } from './components/filter';
 import { FilterProvider } from './components/filter-context';
 import { Squads } from './components/squads';
@@ -17,72 +19,6 @@ export async function generateStaticParams() {
   return [];
 }
 
-const YASB_REGEXP = /https:\/\/yasb\.app\/\?f(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)/;
-
-const getXWS = async (url: string) => {
-  // Currently only supporting YASB links
-  if (!/yasb\.app/.test(url)) {
-    return null;
-  }
-
-  // Get XWS using https://github.com/zacharyp/squad2xws
-  const res = await fetch(
-    url.replace(
-      'https://yasb.app',
-      'https://squad2xws.objectivecat.com/yasb/xws'
-    )
-  );
-
-  if (!res.ok) {
-    throw new Error(`Failed to fetch XWS data for ${url}...`);
-  }
-
-  let xws;
-  try {
-    xws = await res.json();
-  } catch {
-    throw new Error(`Failed to parse JSON for ${url}...`);
-  }
-
-  return xws as XWSSquad;
-};
-
-const getListsFromEvent = async (event: string) => {
-  const res = await fetch(
-    `https://longshanks.org/events/detail/?event=${event}`
-  );
-
-  if (!res.ok) {
-    throw new Error('Failed to fetch event data...');
-  }
-
-  // Poor mans web scraper...
-  const html = await res.text();
-  const matches = html.matchAll(
-    /id=\"list_(?<id>\d+)\" value=\"(?<value>[^"]*)\"/g
-  );
-
-  const lists = await Promise.all(
-    Array.from(matches).map(async m => {
-      const val = m.groups?.value || '';
-      const id = m.groups?.id!;
-      const url = (val.replace(/(\r\n|\n|\r)/gm, '').match(YASB_REGEXP) || [
-        null,
-      ])[0];
-      const xws = await getXWS(url || '');
-
-      return {
-        id,
-        url,
-        xws,
-        raw: val,
-      };
-    })
-  );
-
-  return lists;
-};
-
 export interface PageProps {
   params: {
     event: string;
@@ -90,15 +26,15 @@ export interface PageProps {
 }
 
 const Page = async ({ params }: PageProps) => {
-  const data = await getListsFromEvent(params.event);
-  const dataWithXWS = data.filter(item => Boolean(item.xws)) as {
+  const { title, squads } = await getEvent(params.event);
+  const squadsWithXWS = squads.filter(item => Boolean(item.xws)) as {
     id: string;
     url: string;
     xws: XWSSquad;
     raw: string;
   }[];
 
-  if (dataWithXWS.length === 0) {
+  if (squadsWithXWS.length === 0) {
     return (
       <div className="pt-4">
         <Center>
@@ -115,15 +51,15 @@ const Page = async ({ params }: PageProps) => {
   return (
     <main className="p-4">
       <div>
-        <Title>Event #{params.event}</Title>
+        <Title>{title || `Event #${params.event}`}</Title>
         <Caption>
-          Showing {dataWithXWS.length}/{data.length} lists
+          Showing {squadsWithXWS.length}/{squads.length} lists
         </Caption>
       </div>
       <div className="mx-auto my-4 w-[min(100%_-_3rem,_75rem)]">
         <FilterProvider>
           <Filter />
-          <Squads squads={dataWithXWS} />
+          <Squads squads={squadsWithXWS} />
         </FilterProvider>
       </div>
     </main>
---
+++ lib/longshanks/event.ts
@@ -0,0 +1,59 @@
+import { yasb2xws } from 'lib/xws';
+import { CheerioAPI, load } from 'cheerio';
+
+/**
+ * Scrape event title from meta tag.
+ */
+export const parseTitle = ($: CheerioAPI) =>
+  $('head meta[property=og:title]').attr('content') || null;
+
+/**
+ * Iterate over all player related html and scrape their name
+ * and squad.
+ */
+export const parseSquads = async ($: CheerioAPI) =>
+  await Promise.all(
+    $('[class=pop][id^=details_]')
+      .toArray()
+      .map(async el => {
+        const player = $('.player_link', el).text();
+
+        const list = $('[id^=list_]', el);
+        const id = list.attr('id');
+        const raw = list.attr('value') || '';
+
+        // Get XWS for YASB link
+        const YASB_REGEXP =
+          /https:\/\/yasb\.app\/\?f(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)/;
+        const url = (raw.replace(/(\r\n|\n|\r)/gm, '').match(YASB_REGEXP) || [
+          null,
+        ])[0];
+        const xws = await yasb2xws(url || '');
+
+        return {
+          id,
+          url,
+          xws,
+          raw,
+          player,
+        };
+      })
+  );
+
+export const getEvent = async (event: string) => {
+  const res = await fetch(
+    `https://longshanks.org/events/detail/?event=${event}`
+  );
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch event data...');
+  }
+
+  const html = await res.text();
+  const $ = load(html);
+
+  const title = parseTitle($);
+  const squads = await parseSquads($);
+
+  return { title, squads };
+};
