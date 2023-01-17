623e62eec7324ab5b0bf7f5553c56b69cd42d036
Sebastian Sebald <sebastian.sebald@reservix.de>
2023-01-17 07:00:28 +0100

    refa to use cheerio for scraping

--- lib/longshanks/event.ts
+++ lib/longshanks/event.ts
@@ -1,43 +1,41 @@
 import { yasb2xws } from 'lib/xws';
 import { CheerioAPI, load } from 'cheerio';
 
-const YASB_REGEXP = /https:\/\/yasb\.app\/\?f(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)/;
-
 /**
- * Get event title from meta tag.
+ * Scrape event title from meta tag.
  */
 export const parseTitle = ($: CheerioAPI) =>
   $('head meta[property=og:title]').attr('content') || null;
 
 /**
- * Iterate over all player related html and get their name
+ * Iterate over all player related html and scrape their name
  * and squad.
  */
-export const parsePlayers = async ($: CheerioAPI) =>
+export const parseSquads = async ($: CheerioAPI) =>
   await Promise.all(
     $('[class=pop][id^=details_]')
       .toArray()
       .map(async el => {
-        const name = $('.player_link', el).text();
+        const player = $('.player_link', el).text();
 
         const list = $('[id^=list_]', el);
         const id = list.attr('id');
         const raw = list.attr('value') || '';
 
         // Get XWS for YASB link
+        const YASB_REGEXP =
+          /https:\/\/yasb\.app\/\?f(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)/;
         const url = (raw.replace(/(\r\n|\n|\r)/gm, '').match(YASB_REGEXP) || [
           null,
         ])[0];
         const xws = await yasb2xws(url || '');
 
         return {
-          name,
-          squad: {
-            id,
-            url,
-            xws,
-            raw,
-          },
+          id,
+          url,
+          xws,
+          raw,
+          player,
         };
       })
   );
@@ -55,7 +53,7 @@ export const getEvent = async (event: string) => {
   const $ = load(html);
 
   const title = parseTitle($);
-  const players = await parsePlayers($);
+  const squads = await parseSquads($);
 
-  return { title, players };
+  return { title, squads };
 };
