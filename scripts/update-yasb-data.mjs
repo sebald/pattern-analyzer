5aae54ce0323f20f856f2e2942676355c54ca42e
Sebastian Sebald <sebastian.sebald@reservix.de>
2023-01-18 13:13:40 +0100

    only take relevant data from yasb

--- scripts/update-yasb-data.mjs
+++ scripts/update-yasb-data.mjs
@@ -6,11 +6,21 @@ const TARGET = path.join(PROJECT_ROOT, 'lib/data');
 
 const YASB_FILE_PATH = path.resolve(__dirname, 'yasb.tmp.js');
 
+/**
+ * Get YASB data from the source file
+ */
 const res = await fetch('https://yasb.app/javascripts/xwingcontent.min.js');
 const contents = await res.text();
-
 await fs.outputFile(YASB_FILE_PATH, contents);
 
-const foo = require(YASB_FILE_PATH);
+const data = require(YASB_FILE_PATH).basicCardData();
 
-console.log(foo.basicCardData());
+await fs.outputJson(
+  `${TARGET}/yasb.json`,
+  {
+    pilots: data.pilotsById,
+    ships: data.ships,
+    upgrades: data.upgradesById,
+  },
+  { spaces: 2 }
+);
