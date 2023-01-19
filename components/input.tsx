c91d6a05802a2da765269f3251e10d4e816b51ad
Sebastian Sebald <sebastian.sebald@reservix.de>
2023-01-19 23:39:32 +0100

    validation

--- components/input.tsx
+++ components/input.tsx
@@ -7,13 +7,19 @@ import { Label } from './label';
 // ---------------
 const styles = cva(
   [
-    'block w-full rounded-md shadow-sm border-secondary-200',
-    'hover:border-primary-400',
-    'focus:border-primary-300 focus:ring focus:ring-primary-200 focus:ring-opacity-50',
+    'block w-full rounded-md shadow-sm',
+    'focus:ring focus:ring-opacity-50',
     'disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-500',
   ],
   {
     variants: {
+      variant: {
+        default: [
+          'border-secondary-200 hover:border-primary-400',
+          'focus:border-primary-300 focus:ring-primary-200',
+        ],
+        error: ['border-red-400 focus:border-red-400 focus:ring-red-300'],
+      },
       size: {
         small: 'py-2 text-xs',
         regular: 'py-2',
@@ -21,6 +27,7 @@ const styles = cva(
       },
     },
     defaultVariants: {
+      variant: 'default',
       size: 'regular',
     },
   }
@@ -33,30 +40,34 @@ export interface InputProps
     Omit<InputHTMLAttributes<HTMLInputElement>, 'size'> {
   label?: React.ReactNode;
   htmlSize?: InputHTMLAttributes<HTMLInputElement>['size'];
+  error?: React.ReactNode;
 }
 
 // Component
 // ---------------
 export const Input = ({
   label,
-  size,
   type = 'text',
   htmlSize,
+  error,
+  variant,
+  size,
   ...props
 }: InputProps) => {
   const id = useId();
-
+  console.log(error ? 'error' : variant);
   return (
     <div>
       {label && <Label htmlFor={id}>{label}</Label>}
       <input
         {...props}
         id={id}
-        className={styles({ size })}
+        className={styles({ size, variant: error ? 'error' : variant })}
         autoComplete="off"
         size={htmlSize}
         type={type}
       />
+      {error && <p className="mt-1 text-sm text-red-500">{error}</p>}
     </div>
   );
 };
