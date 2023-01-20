795b2fcbc767441108268859921a95db69e13176
Sebastian Sebald <sebastian.sebald@reservix.de>
2023-01-20 12:37:45 +0100

    small start page improvements

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,10 @@ import { EventForm } from './components/event-form';
 const Home = () => {
   return (
     <div className="grid min-h-screen place-items-center">
-      <div className="flex flex-col gap-5">
+      <div className="flex flex-col items-center gap-7">
+        <h1 className="prose text-5xl font-black text-primary-700">
+          Pattern Analyzer
+        </h1>
         <EventForm />
         <Card>
           <List>
