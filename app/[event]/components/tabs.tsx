040709f09ecf2ca2583ebebd6d813af28960de65
Sebastian Sebald <sebastian.sebald@gmail.com>
2023-01-23 10:05:09 +0100

    2nd tabs version with less purple

--- app/[event]/components/tabs.tsx
+++ app/[event]/components/tabs.tsx
@@ -24,14 +24,14 @@ export const Tabs = ({ labels, children }: TabsProps) => {
   return (
     <Radix.Root defaultValue={tabs[0].id}>
       <Radix.List
-        className="-mb-px flex items-center gap-4 border-b border-b-primary-200 text-sm font-medium"
+        className="-mb-px flex items-center gap-4 border-b border-b-secondary-100 text-sm font-medium"
         aria-label="Switch content"
       >
         {tabs.map(({ id, label }) => (
           <Radix.Trigger
             className={[
               montserrat.className,
-              'relative inline-flex cursor-pointer items-center gap-2 px-5 py-3 text-xl font-medium text-primary-400 hover:text-primary-700',
+              'relative inline-flex cursor-pointer items-center gap-2 px-5 py-3 text-lg font-medium text-secondary-300 hover:text-primary-500',
               'data-active:text-primary-700 data-active:after:absolute data-active:after:left-0 data-active:after:bottom-[-1px] data-active:after:h-0.5 data-active:after:w-full data-active:after:bg-primary-700',
             ].join(' ')}
             key={`${id}-tab`}
