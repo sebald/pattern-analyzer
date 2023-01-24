6133c1d543f4304b95f942ccb5c2835b90f6193f
Sebastian Sebald <sebastian.sebald@reservix.de>
2023-01-24 09:26:28 +0100

    refa: no chartjs!!!

--- app/[event]/components/charts.tsx
+++ app/[event]/components/charts.tsx
@@ -1,93 +1,92 @@
 'use client';
 
-import {
-  Chart as ChartJS,
-  ArcElement,
-  Tooltip,
-  Legend,
-  type ChartData,
-} from 'chart.js';
-import { Doughnut } from 'react-chartjs-2';
-
 import { getFactionName } from 'lib/get-value';
 import { XWSFaction } from 'lib/types';
 import { Card } from 'components';
-
-// Registration
-// ---------------
-ChartJS.register(ArcElement, Tooltip, Legend);
+import { ResponsivePie } from '@nivo/pie';
 
 // Helpers
 // ---------------
 const FACTION_COLORS: { [key in XWSFaction | 'unknown']: string } = {
-  rebelalliance: '#d73737',
+  rebelalliance: '#d7d7d7',
   galacticempire: '#2f4e9f',
-  scumandvillainy: '#b79e33',
-  resistance: '#ffa500',
+  scumandvillainy: '#ca8a04',
+  resistance: '#f59e0b',
   firstorder: '#872424',
-  galacticrepublic: '#a17747',
-  separatistalliance: '#363ca3',
-  unknown: '#e5e7eb',
+  galacticrepublic: '#7f1d1d',
+  separatistalliance: '#3730a3',
+  unknown: '#cbd5e1',
 };
 
 // Props
 // ---------------
 export interface FactionDoughnutProps {
-  value: { [key in XWSFaction | 'unknown']: number };
+  // value: { [key in XWSFaction | 'unknown']: number };
 }
 
 // Component
 // ---------------
-export const FactionDoughnut = ({ value }: FactionDoughnutProps) => {
-  const data = Object.entries(value).reduce(
-    (o, [faction, count]) => {
-      o.labels!.push(
-        faction === 'unknown' ? 'Unknown' : getFactionName(faction as any)
-      );
-      o.datasets[0].data.push(count);
+export const FactionDoughnut = () => {
+  // const data = Object.entries(value).reduce(
+  //   (o, [faction, count]) => {
+  //     o.labels!.push(
+  //       faction === 'unknown' ? 'Unknown' : getFactionName(faction as any)
+  //     );
+  //     o.datasets[0].data.push(count);
 
-      // @ts-expect-error
-      o.datasets[0].backgroundColor.push(FACTION_COLORS[faction as any]);
+  //     // @ts-expect-error
+  //     o.datasets[0].backgroundColor.push(FACTION_COLORS[faction as any]);
 
-      return o;
-    },
-    {
-      labels: [],
-      datasets: [{ data: [], backgroundColor: [] }],
-    } as ChartData<'doughnut', number[], string>
-  );
-  const total = Object.values(value).reduce((t, val) => t + val, 0);
-  console.log(value);
+  //     return o;
+  //   },
+  //   {
+  //     labels: [],
+  //     datasets: [{ data: [], backgroundColor: [] }],
+  //   } as any
+  // );
+  // const total = Object.values(value).reduce((t, val) => t + val, 0);
+  // console.log(value);
   return (
     <Card>
-      <Card.Title>Factions</Card.Title>
+      <Card.Title>Faction Distribution</Card.Title>
       <div className="h-60">
-        <Doughnut
-          options={{
-            plugins: {
-              legend: {
-                position: 'right',
-                onClick: e => e.native?.stopPropagation(),
-              },
-              tooltip: {
-                backgroundColor: '#0f172a',
-                displayColors: false,
-                callbacks: {
-                  label(ctx) {
-                    const percentage = new Intl.NumberFormat('default', {
-                      style: 'percent',
-                      minimumFractionDigits: 2,
-                      maximumFractionDigits: 2,
-                    }).format(ctx.parsed / total);
-
-                    return `${ctx.formattedValue} (${percentage})`;
-                  },
-                },
-              },
+        <ResponsivePie
+          margin={{ top: 20, right: 20, bottom: 20, left: 20 }}
+          data={[
+            {
+              id: 'stylus',
+              label: 'stylus',
+              value: 422,
+              color: 'hsl(152, 70%, 50%)',
             },
-            maintainAspectRatio: false,
-          }}
-          data={data}
+            {
+              id: 'erlang',
+              label: 'erlang',
+              value: 405,
+              color: 'hsl(345, 70%, 50%)',
+            },
+            {
+              id: 'java',
+              label: 'java',
+              value: 216,
+              color: 'hsl(106, 70%, 50%)',
+            },
+            {
+              id: 'php',
+              label: 'php',
+              value: 165,
+              color: 'hsl(236, 70%, 50%)',
+            },
+            {
+              id: 'c',
+              label: 'c',
+              value: 236,
+              color: 'hsl(135, 70%, 50%)',
+            },
+          ]}
+          animate
+          activeOuterRadiusOffset={8}
+          innerRadius={0.6}
         />
       </div>
     </Card>
--- app/[event]/components/stats.tsx
+++ app/[event]/components/stats.tsx
@@ -47,61 +47,9 @@ export const Stats = ({ squads }: StatsProps) => {
   return (
     <div className="grid auto-rows-fr grid-cols-[repeat(auto-fit,_minmax(min(350px,_100%),_1fr))] gap-4">
       <div className="">
-        <Nivio />
+        <FactionDoughnut />
       </div>
       <div className="h-72 bg-green-300">asd</div>
     </div>
   );
 };
-
-{
-  /* <div className="w-full md:w-2/5">
-<div className="flex">
-  <FactionDoughnut value={data.factionDistribution} />
-</div>
-<div className="h-96">
-  <Nivio />
-</div>
-</div> */
-}
-
-const Nivio = () => (
-  <ResponsivePie
-    margin={{ top: 20, right: 20, bottom: 20, left: 20 }}
-    data={[
-      {
-        id: 'stylus',
-        label: 'stylus',
-        value: 422,
-        color: 'hsl(152, 70%, 50%)',
-      },
-      {
-        id: 'erlang',
-        label: 'erlang',
-        value: 405,
-        color: 'hsl(345, 70%, 50%)',
-      },
-      {
-        id: 'java',
-        label: 'java',
-        value: 216,
-        color: 'hsl(106, 70%, 50%)',
-      },
-      {
-        id: 'php',
-        label: 'php',
-        value: 165,
-        color: 'hsl(236, 70%, 50%)',
-      },
-      {
-        id: 'c',
-        label: 'c',
-        value: 236,
-        color: 'hsl(135, 70%, 50%)',
-      },
-    ]}
-    animate
-    activeOuterRadiusOffset={8}
-    innerRadius={0.6}
-  />
-);
