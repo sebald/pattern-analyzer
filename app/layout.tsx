50274f44fa6a02343f09da48303ae0d784b0a4a7
Sebastian Sebald <sebastian.sebald@reservix.de>
2023-01-26 13:10:19 +0100

    started working on the footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'components';
 import { inter } from './fonts';
 import './globals.css';
 
@@ -8,7 +9,12 @@ export interface LayoutProps {
 const Layout = ({ children }: LayoutProps) => (
   <html lang="en" className={inter.className}>
     <head />
-    <body className="bg-primary-50">{children}</body>
+    <body className="bg-primary-50">
+      {children}
+      <div className="mx-4 flex flex-row items-center border-t border-primary-200 px-5 py-2 text-sm text-primary-400">
+        <Link href="/about">About</Link>
+      </div>
+    </body>
   </html>
 );
 
