dfb41370b04a9b823e6ee85e6d0981b60b25c5fb
Sebastian Sebald <sebastian.sebald@gmail.com>
2023-01-27 13:05:27 +0100

    feat: empty state for no lists
    closes #34

--- app/event/[...event]/components/squads.tsx
+++ app/event/[...event]/components/squads.tsx
@@ -5,6 +5,28 @@ import type { SquadsData, XWSSquad } from 'lib/types';
 
 import { useFilter } from './filter-context';
 
+const Empty = () => (
+  <div className="grid h-full place-items-center">
+    <div className="flex flex-col items-center gap-1">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="h-8 w-8 text-secondary-100"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z"
+        />
+      </svg>
+      <div className="text-xs text-secondary-200">No list submitted.</div>
+    </div>
+  </div>
+);
+
 const match = (
   search: string,
   { xws, raw, player }: { xws: XWSSquad | null; raw: string; player: string }
@@ -73,10 +95,12 @@ export const Squads = ({ squads }: SquadsProps) => {
           <Card.Body>
             {squad.xws ? (
               <Squad xws={squad.xws} />
-            ) : (
+            ) : squad.raw ? (
               <div className="whitespace-pre-wrap break-words text-sm text-secondary-500">
-                {squad.raw}
+                squad.raw
               </div>
+            ) : (
+              <Empty />
             )}
           </Card.Body>
           <Card.Footer>
