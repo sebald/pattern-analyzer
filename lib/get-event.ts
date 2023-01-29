f23bdef4c8f29272a42161b87fd343b64e108f73
Sebastian Sebald <sebastian.sebald@gmail.com>
2023-01-29 22:28:37 +0100

    works but need work :D

--- lib/get-event.ts
+++ lib/get-event.ts
@@ -1,10 +1,17 @@
 import { getEvent as getLongShankEvent } from './longshanks';
 import { getEvent as getRollbetterEvent } from './rollbetter';
+import type { SquadData } from './types';
 
 const VENDOR = {
   longshanks: getLongShankEvent,
   rollbetter: getRollbetterEvent,
 };
+
+export interface EvenData {
+  urls: string[];
+  title: string;
+  squads: SquadData[];
+}
 
 export interface GetEventByVendorProps {
   vendor: string;
@@ -19,8 +26,24 @@ export const getEventByVendor = async ({
     throw new Error(`Unsupported vendor "${vendor}..."`);
   }
 
-  const get = VENDOR[vendor as keyof typeof VENDOR];
+  const getEvent = VENDOR[vendor as keyof typeof VENDOR];
 
-  const result = await get(id);
+  // Allow to fetch multiple Ids and merge
+  const events = id.split('%2B').map(getEvent);
+  const data = await Promise.all(events);
+
+  const result = data.reduce(
+    (r, { url, title, squads }) => {
+      r.url.push(url);
+      r.squads.push(...squads);
+      return r;
+    },
+    {
+      url: [],
+      title: 'test',
+      squads: [],
+    } as EvenData
+  );
+  console.log(result.url);
   return result;
 };
