b3dc00e22f81a88ed25a3793b7180ae22a07d5dc
Sebastian Sebald <sebastian.sebald@gmail.com>
2023-01-31 13:35:51 +0100

    feat: suffix for all the oddballs
    closes #40

--- scripts/update-xwing-data.mjs
+++ scripts/update-xwing-data.mjs
@@ -21,6 +21,9 @@ const manifest = read('data/manifest.json');
 const PILOT_SUFFIX = {
   // Ships / Configs
   delta7baethersprite: '7b',
+  'oddball-arc170starfighter': '(ARC-170)',
+  'oddball-btlbywing': '(Y-Wing)',
+  'oddball-nimbusclassvwing': '(V-Wing)',
 
   // Ids (needed because Mauler and Temmin have the same caption ...)
   'poedameron-swz68': 'HoH',
