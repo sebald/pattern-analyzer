139d6c1f463c9f2bfe11b05b580fe389f1783c98
Sebastian Sebald <sebastian.sebald@gmail.com>
2023-02-10 16:03:15 +0100

    fix: adjust logo on small screens

--- app/event/[...event]/layout.tsx
+++ app/event/[...event]/layout.tsx
@@ -6,7 +6,7 @@ export interface LayoutProps {
 
 const Layout = ({ children }: LayoutProps) => (
   <>
-    <nav className="py-1 px-2 text-center leading-none xl:px-6 xl:py-2">
+    <nav className="px-2 py-2 text-center leading-none xl:px-6">
       <Link className="inline-block" href="/">
         <Logo className="h-7 w-7 text-primary-300 hover:text-primary-900 lg:h-9 lg:w-9" />
       </Link>
