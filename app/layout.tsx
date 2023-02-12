179a13c5a78f8132e836e287ed55a0cfcc2cca96
Sebastian Sebald <sebastian.sebald@reservix.de>
2023-02-12 12:12:36 +0100

    feat: add link to source

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,7 @@ const Layout = ({ children }: LayoutProps) => (
       <footer className="mx-auto mt-auto w-[min(100%_-_3rem,_75rem)] pt-8">
         <div className="flex items-center justify-center gap-6 border-t border-primary-100 px-2 pt-2 pb-4 text-sm text-primary-200">
           <Link href="/">Home</Link>
+          <Link href="https://github.com/sebald/pattern-analyzer">Source</Link>
           <Link href="/about">About</Link>
         </div>
       </footer>
