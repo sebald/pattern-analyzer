ac007c6b57a18ae38c4c9ea272448a2f0e3145b8
Sebastian Sebald <sebastian.sebald@gmail.com>
2023-02-17 14:35:22 +0100

    work with newer urls ... wtf

--- lib/lbn.ts
+++ lib/lbn.ts
@@ -4,14 +4,19 @@ export const LBN_URL_REGEXP =
   /https:\/\/launchbaynext\.app\/\?lbx(?:[-a-zA-Z0-9()@:%_*'\\\+.~#?&\/=,]*)/;
 
 export const lbn2xws = (link: string) => {
-  const url = new URL(link);
+  // const url = new URL(link);
+  const url = new URL(
+    "https://launchbaynext.app/?lbx='New%20Squadron'.5.2.1.ll41.'secondsister'.l14.299r.l1.237rrr.lr"
+  );
   const params = Object.fromEntries(url.searchParams.entries()) as Record<
     'lbx',
     string
   >;
+  console.log('============================================================');
   console.log(params.lbx);
-  console.log(deserialize(params.lbx.replace(/\\'/g, '"')));
-  // helpers.serializer.deserialize;
+  console.log(params.lbx.replace(/\\\'/g, "'"));
+  console.log(deserialize(params.lbx.replace(/\\\'/g, "'")));
+  console.log('============================================================');
 
   return null;
 };
