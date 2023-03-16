215c876607ded7277170b6d7967c2c8f761027b7
Sebastian Sebald <sebastian.sebald@gmail.com>
2023-03-16 12:43:33 +0100

    improve typography components

--- components/headline.tsx
+++ components/headline.tsx
@@ -1,16 +1,17 @@
 import { ComponentPropsWithoutRef } from 'react';
 import { cva, VariantProps } from 'class-variance-authority';
+import { montserrat } from '@/app/fonts';
 
 // Styles
 // ---------------
-const styles = cva(['scroll-m-20 tracking-tight'], {
+const styles = cva([`${montserrat.className} scroll-m-20`], {
   variants: {
     level: {
-      '1': 'text-4xl font-extrabold lg:text-5xl',
-      '2': 'pb-5 text-3xl font-semibold first:mt-0',
-      '3': 'pb-5 text-2xl font-semibold',
-      '4': 'text-xl font-semibold',
-      '5': 'text-base font-semibold',
+      '1': 'text-4xl font-extrabold lg:text-5xl tracking-tight',
+      '2': 'pb-5 text-3xl font-bold first:mt-0 tracking-tight',
+      '3': 'pb-5 text-2xl font-bold',
+      '4': 'text-xl font-bold',
+      '5': 'text-base font-bold',
     },
   },
   defaultVariants: {
