22cd405a237faa2210a56896df6021844ba2a53c
Sebastian Sebald <sebastian.sebald@gmail.com>
2023-03-18 18:29:23 +0100

    donut!

--- app/event/[...event]/components/charts/faction-distribution.tsx
+++ app/event/[...event]/components/charts/faction-distribution.tsx
@@ -5,7 +5,7 @@ import { ResponsivePie } from '@nivo/pie';
 import { getFactionName } from '@/lib/get-value';
 import { XWSFaction } from '@/lib/types';
 import { Card } from '@/components';
-import { FACTION_COLORS, toPercentage } from './shared';
+import { FACTION_ABBR, FACTION_COLORS, toPercentage } from './shared';
 
 // Props
 // ---------------
@@ -21,22 +21,14 @@ export const FactionDistribution = ({
   total,
 }: FactionDistributionProps) => {
   const data = (Object.entries(value) as [XWSFaction | 'unknown', number][])
-    .map(([faction, count]) => {
-      // Remove unknown if 0
-      if (faction === 'unknown' && count === 0) {
-        return null;
-      }
-
-      return {
-        id: faction === 'unknown' ? 'Unknown' : getFactionName(faction),
-        label: faction, // Unused? Shouldn't this be the other way around!?
-        value: count,
-        color: FACTION_COLORS[faction],
-      };
-    })
-    .filter(Boolean) as {
-    id: string;
-    label: XWSFaction | 'unknown';
+    .map(([faction, count]) => ({
+      id: faction,
+      value: count,
+      color: FACTION_COLORS[faction],
+    }))
+    // Remove "uknown" if everything was parsed!
+    .filter(({ id, value }) => (id !== 'unknown' ? true : value > 0)) as {
+    id: XWSFaction | 'unknown';
     value: number;
     color: string;
   }[];
@@ -48,13 +40,13 @@ export const FactionDistribution = ({
         <ResponsivePie
           data={data}
           valueFormat={value => toPercentage(value / total)}
-          isInteractive={false}
-          margin={{ top: 20, right: 20, bottom: 40, left: 20 }}
+          arcLinkLabel={({ data }) => FACTION_ABBR[data.id]}
           colors={{ datum: 'data.color' }}
-          activeOuterRadiusOffset={8}
-          innerRadius={0.6}
+          innerRadius={0.5}
           padAngle={0.5}
-          cornerRadius={5}
+          cornerRadius={3}
+          margin={{ top: 20, right: 20, bottom: 20, left: 20 }}
+          isInteractive={false}
           animate
         />
       </div>
@@ -66,7 +58,9 @@ export const FactionDistribution = ({
               className="flex items-center gap-1 text-xs text-secondary-900"
             >
               <div className="h-2 w-2" style={{ background: color }} />
-              <div className="font-medium">{id}:</div>
+              <div className="font-medium">
+                {id === 'unknown' ? 'Unknown' : getFactionName(id)}:
+              </div>
               {value}
             </div>
           ))}
