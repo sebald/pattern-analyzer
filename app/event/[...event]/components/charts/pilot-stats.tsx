88a0cb19c6e59d16e2f312b075547341854a1e94
Sebastian Sebald <sebastian.sebald@gmail.com>
2023-03-30 00:11:57 +0200

    works but unfinished

--- app/event/[...event]/components/charts/pilot-stats.tsx
+++ app/event/[...event]/components/charts/pilot-stats.tsx
@@ -1,6 +1,13 @@
 import { Fragment, useState } from 'react';
 
-import { Card, FactionSelection, Select, ShipIcon, Table } from '@/components';
+import {
+  Card,
+  Collapsible,
+  FactionSelection,
+  Select,
+  ShipIcon,
+  Table,
+} from '@/components';
 import type { XWSFaction } from '@/lib/types';
 import { getPilotName } from '@/lib/get-value';
 import { PilotStatData, toPercentage } from './shared';
@@ -49,48 +56,50 @@ export const PilotStats = ({ value }: PilotStatsProps) => {
         </Card.Actions>
       </Card.Header>
       <Card.Body>
-        <Table
-          cols={[
-            'minmax(auto, max-content)',
-            '1fr',
-            '1fr',
-            '1fr',
-            'minmax(90px, 1fr)',
-            '70px',
-          ]}
-          headers={[
-            'Pilot',
-            'Percentile',
-            'Std. Deviation',
-            'Winrate',
-            'Frequency',
-            'Count',
-          ]}
-        >
-          {data.map(([pilot, stat]) => (
-            <Fragment key={pilot}>
-              <Table.Cell variant="header">
-                <ShipIcon ship={stat.ship} className="w-5 text-xl" />
-                <div className="text-sm font-semibold">
-                  {getPilotName(pilot) || pilot}
-                </div>
-              </Table.Cell>
-              <Table.Cell variant="number">
-                {toPercentage(stat.percentile)}
-              </Table.Cell>
-              <Table.Cell variant="number">
-                {stat.deviation === 0 ? '-' : toPercentage(stat.deviation)}
-              </Table.Cell>
-              <Table.Cell variant="number">
-                {toPercentage(stat.winrate)}
-              </Table.Cell>
-              <Table.Cell variant="number">
-                {toPercentage(stat.frequency)}
-              </Table.Cell>
-              <Table.Cell variant="number">{stat.count}</Table.Cell>
-            </Fragment>
-          ))}
-        </Table>
+        <Collapsible maxHeight={500}>
+          <Table
+            cols={[
+              'minmax(auto, max-content)',
+              '1fr',
+              '1fr',
+              '1fr',
+              'minmax(90px, 1fr)',
+              '70px',
+            ]}
+            headers={[
+              'Pilot',
+              'Percentile',
+              'Std. Deviation',
+              'Winrate',
+              'Frequency',
+              'Count',
+            ]}
+          >
+            {data.map(([pilot, stat]) => (
+              <Fragment key={pilot}>
+                <Table.Cell variant="header">
+                  <ShipIcon ship={stat.ship} className="w-5 text-xl" />
+                  <div className="text-sm font-semibold">
+                    {getPilotName(pilot) || pilot}
+                  </div>
+                </Table.Cell>
+                <Table.Cell variant="number">
+                  {toPercentage(stat.percentile)}
+                </Table.Cell>
+                <Table.Cell variant="number">
+                  {stat.deviation === 0 ? '-' : toPercentage(stat.deviation)}
+                </Table.Cell>
+                <Table.Cell variant="number">
+                  {toPercentage(stat.winrate)}
+                </Table.Cell>
+                <Table.Cell variant="number">
+                  {toPercentage(stat.frequency)}
+                </Table.Cell>
+                <Table.Cell variant="number">{stat.count}</Table.Cell>
+              </Fragment>
+            ))}
+          </Table>
+        </Collapsible>
       </Card.Body>
     </Card>
   );
--- components/collapsible.tsx
+++ components/collapsible.tsx
@@ -1,17 +1,60 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { Button } from './button';
 
 export interface CollapsibleProps {
   defaultCollapsed?: boolean;
-  height: string;
+  maxHeight: number;
   children: React.ReactNode;
 }
 
 export const Collapsible = ({
   defaultCollapsed = true,
-  height,
+  maxHeight,
   children,
 }: CollapsibleProps) => {
-  const [collapsed, setCollapsed] = useState(defaultCollapsed);
+  const ref = useRef<HTMLDivElement>(null);
+  const height = useRef(-1);
+  const [collapsed, setCollapsed] = useState(false);
+
+  /**
+   * Run the initial setup only once, this means that
+   * `maxHeight` and `defaultCollapsed` can not be updated
+   * dynamically.
+   */
+  useEffect(() => {
+    height.current = ref.current?.clientHeight || 0;
+    if (defaultCollapsed && height.current > maxHeight) {
+      setCollapsed(true);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const styles = {
+    '--collapsible-height': `${maxHeight}px`,
+  } as React.CSSProperties;
+
+  return (
+    <div className="flex flex-col gap-4">
+      <div
+        ref={ref}
+        style={styles}
+        className={
+          collapsed ? 'max-h-[var(--collapsible-height)] overflow-hidden' : ''
+        }
+      >
+        {children}
+      </div>
+      <div className="flex justify-center">
+        <Button
+          variant="link"
+          size="inherit"
+          onClick={() => setCollapsed(!collapsed)}
+        >
+          {collapsed ? 'Show more' : 'Show less'}
+        </Button>
+      </div>
+    </div>
+  );
 };
