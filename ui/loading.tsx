22154259aa088921d60e69309a2079b0fdf2cd41
Sebastian Sebald <sebastian.sebald@gmail.com>
2023-04-02 18:03:05 +0200

    better loading with logo

--- ui/loading.tsx
+++ ui/loading.tsx
@@ -1,6 +1,7 @@
 import { cn } from '@/lib/utils';
+
 import { Card } from './card';
-import { Logo } from './logo';
+import { Logo, LogoProps } from './logo';
 
 const Content = ({ lines = 3 }) => (
   <div className="flex flex-col gap-1.5">
@@ -12,23 +13,54 @@ const Content = ({ lines = 3 }) => (
   </div>
 );
 
-export interface CardSkeletonProps {
-  lines?: [number, number, number];
-}
+const Stat = () => (
+  <div>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="h-6 w-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 013 19.875v-6.75zM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V8.625zM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V4.125z"
+      />
+    </svg>
+  </div>
+);
 
 // Loading: Logo
 // ---------------
-export const LogoSkeleton = ({ className }: { className?: string }) => (
-  <Logo className={cn('h-20 w-20 animate-pulse', className)} />
+export const LogoSkeleton = ({ className, ...props }: LogoProps) => (
+  <Logo
+    className={cn('h-20 w-20 animate-pulse', className)}
+    strokeWidth={1.5}
+    {...props}
+  />
 );
 
 // Loading: Card
 // ---------------
+export interface CardSkeletonProps {
+  lines?: [number, number, number];
+}
+
 export const CardSkeleton = ({ lines = [3, 2, 2] }: CardSkeletonProps) => (
   <Card role="status" className="animate-pulse" elevation="light">
     <Content lines={lines[0]} />
     <Content lines={lines[1]} />
     <Content lines={lines[2]} />
+  </Card>
+);
+
+// Loading: Stats
+// ---------------
+export const StatsSkeleton = () => (
+  <Card role="status" className="animate-pulse" elevation="light">
+    asd
   </Card>
 );
 
