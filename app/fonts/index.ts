7fd074f267c452886bd24678177f44df63e0efc3
Sebastian Sebald <sebastian.sebald@gmail.com>
2023-04-03 23:06:58 +0200

    use variables for fonts

--- app/fonts/index.ts
+++ app/fonts/index.ts
@@ -1,6 +1,9 @@
 import { Inter, Montserrat } from 'next/font/google';
 import LocalFont from 'next/font/local';
 
-export const inter = Inter({ subsets: ['latin'] });
-export const montserrat = Montserrat({ subsets: ['latin'] });
+export const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
+export const montserrat = Montserrat({
+  subsets: ['latin'],
+  variable: '--font-montserrat',
+});
 export const xwingShips = LocalFont({ src: './xwing-miniatures-ships.ttf' });
