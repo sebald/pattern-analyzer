0ea4931658e2cdde2a5bbb9b66c604d8ddb4758d
Sebastian Sebald <sebastian.sebald@gmail.com>
2023-04-11 21:43:42 +0200

    setup to have a title

--- app/api/og/helpers.tsx
+++ app/api/og/helpers.tsx
@@ -1,3 +1,21 @@
+export const Logo = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="#3c4073"
+    stroke-width="2"
+    width="256"
+    height="256"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M7.5 14.25v2.25m3-4.5v4.5m3-6.75v6.75m3-9v9M6 20.25h12A2.25 2.25 0 0020.25 18V6A2.25 2.25 0 0018 3.75H6A2.25 2.25 0 003.75 6v12A2.25 2.25 0 006 20.25z"
+    />
+  </svg>
+);
+
 // Stolen from : https://www.jacobparis.com/guides/remix-og
 export const getGoogleFont = async (
   font: string,
@@ -29,5 +47,5 @@ export const getGoogleFont = async (
         weight: weights[i],
         data: await buffer,
       }))
-  );
+  ) as any;
 };
--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -1,6 +1,6 @@
 import { type NextRequest, ImageResponse } from 'next/server';
 
-import { Analyzer, Logo, Pattern } from './components';
+import { Logo, getGoogleFont } from './helpers';
 
 // Handler
 // ---------------
@@ -8,33 +8,58 @@ export const GET = async (req: NextRequest) => {
   const url = new URL(req.url);
   const title = url.searchParams.get('title');
 
-  if (!title) {
-    return new ImageResponse(
-      (
-        <div
-          style={{
-            fontSize: 128,
-            width: '100%',
-            height: '100%',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            backgroundColor: '#f1f5fc',
-            background: 'radial-gradient(circle, #b4c5ed 5%, transparent 6%)',
-            backgroundSize: '50px 50px',
-          }}
-        >
-          <Logo />
-          <div style={{ display: 'flex', flexDirection: 'column', gap: 4 }}>
-            <Pattern />
-            <Analyzer />
-          </div>
-        </div>
-      ),
-      {
-        width: 1200,
-        height: 630,
-      }
-    );
-  }
+  const fonts = await Promise.all([
+    getGoogleFont('Montserrat', [900]),
+    getGoogleFont('Inter'),
+  ]).then(fonts => fonts.flat());
+
+  const content = title ? (
+    'hello'
+  ) : (
+    <>
+      <Logo />
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          gap: 0,
+          color: '#3c4073',
+          fontFamily: 'Montserrat',
+          fontWeight: 900,
+          textTransform: 'uppercase',
+          lineHeight: 1,
+          fontSize: 80,
+        }}
+      >
+        <span>Pattern</span>
+        <span>Analyzer</span>
+      </div>
+    </>
+  );
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          width: '100%',
+          height: '100%',
+          fontFamily: 'Inter',
+          backgroundColor: '#f1f5fc',
+          background: 'radial-gradient(circle, #b4c5ed 5%, transparent 6%)',
+          backgroundSize: '50px 50px',
+        }}
+      >
+        {content}
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 630,
+      fonts,
+    }
+  );
 };
