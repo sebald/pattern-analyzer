c7e104f547eceaf2a3c3acaedcd4f0db5d9ddff3
Sebastian Sebald <sebastian.sebald@gmail.com>
2023-04-17 06:20:42 +0200

    paginate request ...

--- app/api/longshanks/[event]/players/route.ts
+++ app/api/longshanks/[event]/players/route.ts
@@ -7,9 +7,28 @@ import { parsePlayerInfo, parseSquads } from '@/lib/longshanks';
 // ---------------
 export const revalidate = 86_400; // 1 day
 
-const schema = z.object({
-  event: z.string().regex(/^[0-9]+$/),
-});
+const schema = {
+  params: z.object({
+    event: z.string().regex(/^[0-9]+$/),
+  }),
+  searchParams: z.object({
+    page: z
+      .string()
+      .regex(/^[0-9]+$/)
+      .nullable()
+      .transform(val => (val === null ? 1 : Number(val))),
+    size: z
+      .string()
+      .regex(/^[0-9]+$/)
+      .nullable()
+      .transform(val => (val === null ? 50 : Number(val))),
+  }),
+};
+
+const paginate = <T extends any[]>(
+  items: T,
+  { page, size }: { page: number; size: number }
+) => items.slice(size * (page - 1), size * page);
 
 // Props
 // ---------------
@@ -21,14 +40,13 @@ interface RouteContext {
 
 // Handler
 // ---------------
-export const GET = async (_: NextRequest, { params }: RouteContext) => {
-  const result = schema.safeParse(params);
-
-  if (!result.success) {
+export const GET = async (request: NextRequest, { params }: RouteContext) => {
+  const event = schema.params.safeParse(params);
+  if (!event.success) {
     return NextResponse.json(
       {
         name: 'Error parsing input.',
-        message: result.error.issues,
+        message: event.error.issues,
       },
       {
         status: 400,
@@ -36,10 +54,29 @@ export const GET = async (_: NextRequest, { params }: RouteContext) => {
     );
   }
 
-  const id = result.data.event;
+  // Pagination
+  const { searchParams } = new URL(request.url);
+  const pagination = schema.searchParams.safeParse({
+    page: searchParams.get('page'),
+    size: searchParams.get('size'),
+  });
+
+  if (!pagination.success) {
+    return NextResponse.json(
+      {
+        name: 'Error parsing input.',
+        message: pagination.error.issues,
+      },
+      {
+        status: 400,
+      }
+    );
+  }
+
+  const id = event.data.event;
 
   const players = await parsePlayerInfo(id);
-  const squads = await parseSquads(id, players);
+  const squads = await parseSquads(id, paginate(players, pagination.data));
 
   return NextResponse.json(squads);
 };
