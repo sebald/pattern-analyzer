320dd5bc5872e5ada4f37622e29da18f0d9cc350
Sebastian Sebald <sebastian.sebald@gmail.com>
2023-06-21 08:49:39 +0200

    dont preload

--- app/preload.ts
+++ app/preload.ts
@@ -3,35 +3,4 @@ import { Vendor } from '@/lib/types';
 export const RECENT_EVENTS: {
   vendor: Vendor;
   eventId: string;
-}[] = [
-  {
-    // Worlds
-    eventId: '3620',
-    vendor: 'listfortress',
-  },
-  {
-    // Worlds LCQ
-    eventId: '3619',
-    vendor: 'listfortress',
-  },
-  {
-    // Sith Taker Open
-    eventId: '3629',
-    vendor: 'listfortress',
-  },
-  {
-    // German TTS Leagure
-    eventId: '6861',
-    vendor: 'longshanks',
-  },
-  {
-    // LVO Day 2
-    eventId: '57',
-    vendor: 'rollbetter',
-  },
-  {
-    // LVO Day 1
-    eventId: '56',
-    vendor: 'rollbetter',
-  },
-];
+}[] = [];
