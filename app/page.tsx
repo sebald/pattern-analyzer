f8b615a468bd53c2e45d625f1a60ebbe877fc0d8
Sebastian Sebald <sebastian.sebald@gmail.com>
2023-07-06 09:45:42 +0200

    woring on the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -34,47 +34,39 @@ const Home = async () => {
   const events = await getReventEvents();
 
   return (
-    <Container className="grid flex-1 place-items-center">
-      <div>
-        <h1
-          className={cn(
-            headline.variable,
-            'prose flex items-center justify-center pb-20 pt-8 font-headline text-2xl font-extrabold uppercase text-primary-900 md:pb-14'
-          )}
-        >
-          <Logo className="h-10 w-10" />
-          Pattern Analyzer
-        </h1>
-        <div className="flex-1 pb-24">
-          <EventForm />
-        </div>
-        <div className="w-full md:px-6">
-          <Headline level="3" className="text-primary-800">
-            Recent Events
-          </Headline>
-          <Card>
-            <Collapsible maxHeight={350}>
-              <List variant="wide">
-                {events.map(({ id, name, date, country }) => (
-                  <List.Item key={id}>
-                    <Link
-                      className="text-lg text-secondary-900"
-                      href={`/event/listfortress/${id}`}
-                    >
-                      <h3 className="font-medium">{name}</h3>
-                      <div className="text-sm text-secondary-400">
-                        {date}
-                        {country ? `, ${country}` : ''}
-                      </div>
-                    </Link>
-                  </List.Item>
-                ))}
-              </List>
-            </Collapsible>
-          </Card>
-        </div>
+    <div className="container grid flex-1 place-items-center">
+      <div className="flex-1 pb-24">
+        <Headline level="3" className="text-primary-800">
+          Find an Event:
+        </Headline>
+        <EventForm />
       </div>
-    </Container>
+      <div className="w-full md:px-6">
+        <Headline level="3" className="text-primary-800">
+          Recent Events
+        </Headline>
+        <Card>
+          <Collapsible maxHeight={350}>
+            <List variant="wide">
+              {events.map(({ id, name, date, country }) => (
+                <List.Item key={id}>
+                  <Link
+                    className="text-lg text-secondary-900"
+                    href={`/event/listfortress/${id}`}
+                  >
+                    <h3 className="font-medium">{name}</h3>
+                    <div className="text-sm text-secondary-400">
+                      {date}
+                      {country ? `, ${country}` : ''}
+                    </div>
+                  </Link>
+                </List.Item>
+              ))}
+            </List>
+          </Collapsible>
+        </Card>
+      </div>
+    </div>
   );
 };
 
