625e08afb654dd35d48e4a986d6b5bc334eeab9e
Sebastian Sebald <sebastian.sebald@gmail.com>
2023-07-06 23:06:42 +0200

    feat: set `metadataBase`

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,7 @@ export const metadata = {
   appleWebApp: {
     title: 'Pattern Analyzer',
   },
+  metadataBase: new URL('https://www.pattern-analyzer.app/'),
   openGraph: {
     siteName: 'Pattern Analyzer',
     title: 'Home',
