d6517539ff282a94b1a1931712904f2a0926cc02
Sebastian Sebald <sebastian.sebald@gmail.com>
2023-07-06 09:39:44 +0200

    improve footer

--- app/components/site-footer.tsx
+++ app/components/site-footer.tsx
@@ -2,13 +2,7 @@ import Image from 'next/image';
 
 export const SiteFooter = () => (
   <footer className="mt-10 border border-b border-primary-200 px-4 py-6">
-    <div className="container flex items-center gap-8">
-      <Image
-        src="/powered-by-vercel.svg"
-        alt="Powered by Vercel"
-        width={159}
-        height={33}
-      />
+    <div className="container flex items-center justify-between gap-8">
       <p className="text-sm text-secondary-600">
         The source code is available on{' '}
         <a
@@ -21,6 +15,18 @@ export const SiteFooter = () => (
         </a>
         .
       </p>
+      <a
+        href="https://vercel.com/?utm_source=sebald&utm_campaign=oss"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <Image
+          src="/powered-by-vercel.svg"
+          alt="Powered by Vercel"
+          width={159}
+          height={33}
+        />
+      </a>
     </div>
   </footer>
 );
