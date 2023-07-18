a2ca4df8c1c881e3f115cde6d7c8b44fc52d802f
Sebastian Sebald <sebastian.sebald@gmail.com>
2023-07-18 10:49:45 +0200

    fix: update tests downgrade prettier

--- __test__/lib/stats/magic.test.ts
+++ __test__/lib/stats/magic.test.ts
@@ -5,11 +5,11 @@ import { magic } from '@/lib/stats/magic';
  */
 test('it is magic!', () => {
   const input = { percentile: 0.5, deviation: 0.1 };
-  expect(magic({ ...input, count: 1 })).toMatchInlineSnapshot(`1.2`);
-  expect(magic({ ...input, count: 2 })).toMatchInlineSnapshot(`2.77`);
-  expect(magic({ ...input, count: 10 })).toMatchInlineSnapshot(`9.21`);
-  expect(magic({ ...input, count: 20 })).toMatchInlineSnapshot(`11.98`);
-  expect(magic({ ...input, count: 50 })).toMatchInlineSnapshot(`15.65`);
-  expect(magic({ ...input, count: 100 })).toMatchInlineSnapshot(`18.42`);
-  expect(magic({ ...input, count: 200 })).toMatchInlineSnapshot(`21.19`);
+  expect(magic({ ...input, count: 1 })).toMatchInlineSnapshot(`1.5`);
+  expect(magic({ ...input, count: 2 })).toMatchInlineSnapshot(`3.47`);
+  expect(magic({ ...input, count: 10 })).toMatchInlineSnapshot(`11.51`);
+  expect(magic({ ...input, count: 20 })).toMatchInlineSnapshot(`14.98`);
+  expect(magic({ ...input, count: 50 })).toMatchInlineSnapshot(`19.56`);
+  expect(magic({ ...input, count: 100 })).toMatchInlineSnapshot(`23.03`);
+  expect(magic({ ...input, count: 200 })).toMatchInlineSnapshot(`26.49`);
 });
