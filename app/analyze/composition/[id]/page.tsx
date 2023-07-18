d6847d59af322c3c9600b75fb0d8c3db7188181c
Sebastian Sebald <sebastian.sebald@gmail.com>
2023-07-18 13:30:35 +0200

    list squads

--- app/analyze/composition/[id]/page.tsx
+++ app/analyze/composition/[id]/page.tsx
@@ -5,7 +5,7 @@ import { toPercentage } from '@/lib/utils';
 import { fromDate } from '@/lib/utils/date.utils';
 import { getAllTournaments, getSquads } from '@/lib/vendor/listfortress';
 
-import { Card, Detail, ShipIcon } from '@/ui';
+import { Card, Detail, ShipIcon, Squad } from '@/ui';
 
 import { TrendCurve } from './_component/trend-curve';
 
@@ -74,46 +74,42 @@ const Page = async ({ params }: PageParams) => {
         <Card.Header>
           <Card.Title>Overview</Card.Title>
         </Card.Header>
-        <Card.Body className="px-3">
-          <div className="grid divide-y md:grid-cols-2 md:divide-x md:divide-y-0">
-            <div className="flex flex-col pb-5 md:row-span-3 md:pb-0 md:pt-2">
-              <Detail
-                label="Ships"
-                value={stats.ships.map((ship, idx) => (
-                  <div
-                    key={idx}
-                    className="flex items-center gap-2 text-secondary-700"
-                  >
-                    <ShipIcon key={idx} ship={ship} className="text-3xl" />
-                    <span className="text-sm font-medium">
-                      {getShipName(ship)}
-                    </span>
-                  </div>
-                ))}
-              />
-            </div>
-            <div className="py-5 md:col-start-2 md:pb-3 md:pl-8 md:pt-2">
-              <Detail
-                label="Percentile"
-                value={`${toPercentage(stats.percentile)} (${toPercentage(
-                  stats.deviation
-                )})`}
-              />
-            </div>
-            <div className="py-5 md:col-start-2 md:py-3 md:pl-8">
-              <Detail
-                label="Winrate"
-                value={
-                  stats.winrate !== null ? toPercentage(stats.winrate) : '-'
-                }
-              />
-            </div>
-            <div className="py-5 md:col-start-2 md:pl-8 md:pt-3">
-              <Detail
-                label="Frequency (Overall)"
-                value={`${toPercentage(stats.frequency)} (${stats.count})`}
-              />
-            </div>
+        <Card.Body className="grid divide-y px-3 md:grid-cols-2 md:divide-x md:divide-y-0">
+          <div className="flex flex-col pb-5 md:row-span-3 md:pb-0 md:pt-2">
+            <Detail
+              label="Ships"
+              value={stats.ships.map((ship, idx) => (
+                <div
+                  key={idx}
+                  className="flex items-center gap-2 text-secondary-700"
+                >
+                  <ShipIcon key={idx} ship={ship} className="text-3xl" />
+                  <span className="text-sm font-medium">
+                    {getShipName(ship)}
+                  </span>
+                </div>
+              ))}
+            />
+          </div>
+          <div className="py-5 md:col-start-2 md:pb-3 md:pl-8 md:pt-2">
+            <Detail
+              label="Percentile"
+              value={`${toPercentage(stats.percentile)} (${toPercentage(
+                stats.deviation
+              )})`}
+            />
+          </div>
+          <div className="py-5 md:col-start-2 md:py-3 md:pl-8">
+            <Detail
+              label="Winrate"
+              value={stats.winrate !== null ? toPercentage(stats.winrate) : '-'}
+            />
+          </div>
+          <div className="py-5 md:col-start-2 md:pl-8 md:pt-3">
+            <Detail
+              label="Frequency (Overall)"
+              value={`${toPercentage(stats.frequency)} (${stats.count})`}
+            />
           </div>
         </Card.Body>
       </Card>
@@ -122,6 +118,21 @@ const Page = async ({ params }: PageParams) => {
           <Card.Title>Trend (in Percentile)</Card.Title>
           <Card.Body>
             <TrendCurve value={stats.trend} />
+          </Card.Body>
+        </Card.Header>
+      </Card>
+      <Card className="col-span-full px-0">
+        <Card.Header>
+          <Card.Title>Squads</Card.Title>
+          <Card.Body className="grid divide-y">
+            {stats.squads.map(({ player, event, xws }) => (
+              <div
+                key={player + event.date + event.rank.swiss}
+                className="px-8 py-6"
+              >
+                <Squad variant="narrow" size="small" xws={xws} />
+              </div>
+            ))}
           </Card.Body>
         </Card.Header>
       </Card>
--- ui/squad.tsx
+++ ui/squad.tsx
@@ -1,5 +1,44 @@
+import { VariantProps, cva } from 'class-variance-authority';
+
 import { getPilotName, getShipName, getUpgradeName } from '@/lib/get-value';
 import type { XWSSquad, XWSUpgrades } from '@/lib/types';
+
+// Helper
+// ---------------
+const styles = {
+  container: cva('flex flex-col', {
+    variants: {
+      variant: {
+        narrow: 'gap-2.5',
+        default: 'gap-4',
+      },
+      size: {
+        default: '',
+        small: '',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+      size: 'default',
+    },
+  }),
+  pilot: cva('flex items-center font-semibold text-secondary-900', {
+    variants: {
+      variant: {
+        narrow: '',
+        default: 'pb-1',
+      },
+      size: {
+        default: '',
+        small: 'text-sm',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+      size: 'default',
+    },
+  }),
+};
 
 const upgradesToList = (upgrades: XWSUpgrades) =>
   (Object.entries(upgrades) as [keyof XWSUpgrades, string[]][])
@@ -7,19 +46,23 @@ const upgradesToList = (upgrades: XWSUpgrades) =>
     .flat()
     .join(', ');
 
-export interface SquadProps {
+// Props
+// ---------------
+export interface SquadProps extends VariantProps<typeof styles.container> {
   xws: XWSSquad;
 }
 
-export const Squad = ({ xws }: SquadProps) => {
+// Component
+// ---------------
+export const Squad = ({ variant, size, xws }: SquadProps) => {
   const { pilots } = xws;
 
   return (
-    <div className="flex flex-col gap-4">
+    <div className={styles.container({ variant, size })}>
       {pilots.map(({ id, ship, upgrades }, idx) => (
         <div key={`${id}-${idx}`}>
           <div
-            className="flex items-center pb-1 font-semibold text-secondary-900"
+            className={styles.pilot({ variant, size })}
             title={`Ship: ${getShipName(ship) || ship}`}
           >
             {getPilotName(id) || id}
