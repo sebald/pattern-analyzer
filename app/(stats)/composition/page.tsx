fdc2113fb3d9c0d9e0dc4903c4f0038e7a98f28f
Sebastian Sebald <sebastian.sebald@gmail.com>
2023-11-09 13:08:16 +0100

    refa content out of compositions

--- app/(stats)/composition/page.tsx
+++ app/(stats)/composition/page.tsx
@@ -1,23 +1,17 @@
 import { createMetadata } from '@/lib/metadata';
-import { getFactionCount, getSquads } from '@/lib/db/squads';
-import { getTournamentsCount } from '@/lib/db/tournaments';
 
-import { Caption, CardTableSkeleton, Message, Title } from '@/ui';
-
-import { StatsHint } from '@/ui/stats/stats-hint';
-import { setup } from '@/lib/stats';
-import { CompositionData, composition } from '@/lib/stats/module';
+import { Caption, Message, Title } from '@/ui';
 
 import { DateRangeFilter } from '@/ui/params/date-range-filter';
 import { FactionFilter } from '@/ui/params/faction-filter';
 import { SmallSamplesFilter } from '@/ui/params/small-samples-filter';
 import { SortParam } from '@/ui/params/sort-param';
 
-import { Compositions } from './compositions';
-import { Suspense } from 'react';
 import { toDateRange } from '@/lib/utils/params.utils';
 import { Filter } from '@/ui/params/filter';
 import { StatsInfo } from '@/ui/stats/stats-info';
+
+import { Content } from './content';
 
 // Metadata
 // ---------------
@@ -27,26 +21,6 @@ export const metadata = createMetadata({
   ogTitle: 'Squad Compositions',
   ogWidth: 65,
 });
-
-// Helpers
-// ---------------
-const create = setup<CompositionData>([composition]);
-
-// Content
-// ---------------
-const Content = async ({ from, to }: { from: Date; to?: Date }) => {
-  const [squads, tournaments, count] = await Promise.all([
-    getSquads({ from, to }),
-    getTournamentsCount({ from, to }),
-    getFactionCount({ from, to }),
-  ]);
-  const stats = create(squads, {
-    count,
-    tournaments,
-  });
-
-  return <Compositions data={stats.composition} />;
-};
 
 // Props
 // ---------------
@@ -89,16 +63,7 @@ const CompositionsPage = async ({ searchParams }: PageProps) => {
         <FactionFilter />
         <SortParam />
       </Filter>
-      <Suspense fallback={<CardTableSkeleton />}>
-        <div className="grid grid-cols-1 gap-4 md:grid-cols-12">
-          <div className="col-span-full">
-            <Content to={to} from={from} />
-          </div>
-          <div className="col-span-full pt-8 lg:col-start-2 lg:col-end-12">
-            <StatsHint />
-          </div>
-        </div>
-      </Suspense>
+      <Content from={from} to={to} />
     </>
   );
 };
