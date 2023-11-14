a597dfb2870156b33b6acc6c8a6943d9eed53d1b
Sebastian Sebald <sebastian.sebald@gmail.com>
2023-11-14 23:08:15 +0100

    refa how to use collapsible

--- ui/stats/composition-stats/composition-table.tsx
+++ ui/stats/composition-stats/composition-table.tsx
@@ -43,75 +43,79 @@ export const CompositionTable = ({
       : b.percentile - a.percentile;
   });
 
-  return (
-    <Collapsible maxHeight={800} disabled={!collapsible}>
-      <Table
-        cols={[
-          'max-content',
-          'max-content',
-          '1fr',
-          '1fr',
-          '1fr',
-          '1fr',
-          '1fr',
-          '1fr',
-          'max-content',
-        ]}
-        headers={[
-          'Ships',
-          'Faction',
-          'Percentile',
-          'Std. Deviation',
-          'Winrate',
-          'Frequency',
-          'Count',
-          'Score',
-          'View',
-        ]}
-        numeration
-      >
-        {data.map(([id, stat]) => (
-          <Fragment key={id}>
-            <Table.Cell
-              variant="header"
-              className="flex w-full items-center gap-1 "
+  const table = (
+    <Table
+      cols={[
+        'max-content',
+        'max-content',
+        '1fr',
+        '1fr',
+        '1fr',
+        '1fr',
+        '1fr',
+        '1fr',
+        'max-content',
+      ]}
+      headers={[
+        'Ships',
+        'Faction',
+        'Percentile',
+        'Std. Deviation',
+        'Winrate',
+        'Frequency',
+        'Count',
+        'Score',
+        'View',
+      ]}
+      numeration
+    >
+      {data.map(([id, stat]) => (
+        <Fragment key={id}>
+          <Table.Cell
+            variant="header"
+            className="flex w-full items-center gap-1 "
+          >
+            {stat.ships.map((ship, idx) => (
+              <ShipIcon key={idx} ship={ship} className="text-2xl" />
+            ))}
+          </Table.Cell>
+          <Table.Cell>
+            <FactionIcon
+              faction={stat.faction}
+              className="h-5 w-5 text-secondary-700"
+            />
+          </Table.Cell>
+          <Table.Cell variant="number">
+            {toPercentage(stat.percentile)}
+          </Table.Cell>
+          <Table.Cell variant="number">
+            {stat.deviation === 0 ? '-' : toPercentage(stat.deviation)}
+          </Table.Cell>
+          <Table.Cell variant="number">
+            {stat.winrate !== null ? toPercentage(stat.winrate) : '-'}
+          </Table.Cell>
+          <Table.Cell variant="number">
+            {toPercentage(stat.frequency)}
+          </Table.Cell>
+          <Table.Cell variant="number">{stat.count}</Table.Cell>
+          <Table.Cell variant="number">{stat.score.toFixed(2)}</Table.Cell>
+          <Table.Cell className="justify-center">
+            <Link
+              href={`/composition/${id}`}
+              variant="highlight"
+              className="text-primary-800"
             >
-              {stat.ships.map((ship, idx) => (
-                <ShipIcon key={idx} ship={ship} className="text-2xl" />
-              ))}
-            </Table.Cell>
-            <Table.Cell>
-              <FactionIcon
-                faction={stat.faction}
-                className="h-5 w-5 text-secondary-700"
-              />
-            </Table.Cell>
-            <Table.Cell variant="number">
-              {toPercentage(stat.percentile)}
-            </Table.Cell>
-            <Table.Cell variant="number">
-              {stat.deviation === 0 ? '-' : toPercentage(stat.deviation)}
-            </Table.Cell>
-            <Table.Cell variant="number">
-              {stat.winrate !== null ? toPercentage(stat.winrate) : '-'}
-            </Table.Cell>
-            <Table.Cell variant="number">
-              {toPercentage(stat.frequency)}
-            </Table.Cell>
-            <Table.Cell variant="number">{stat.count}</Table.Cell>
-            <Table.Cell variant="number">{stat.score.toFixed(2)}</Table.Cell>
-            <Table.Cell className="justify-center">
-              <Link
-                href={`/composition/${id}`}
-                variant="highlight"
-                className="text-primary-800"
-              >
-                <Folder className="h-5 w-5" />
-              </Link>
-            </Table.Cell>
-          </Fragment>
-        ))}
-      </Table>
-    </Collapsible>
+              <Folder className="h-5 w-5" />
+            </Link>
+          </Table.Cell>
+        </Fragment>
+      ))}
+    </Table>
+  );
+
+  return collapsible ? (
+    <Collapsible maxHeight={800}>{table}</Collapsible>
+  ) : (
+    table
   );
 };
