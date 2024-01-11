c40cc8243ccefa2bb355b5ac32b2eadb8074300c
Sebastian Sebald <sebastian.sebald@gmail.com>
2024-01-11 09:18:17 +0100

    refa table?

---
+++ ui/table-next.tsx
@@ -0,0 +1,159 @@
+import {
+  Children,
+  cloneElement,
+  createContext,
+  forwardRef,
+  isValidElement,
+} from 'react';
+import type { ReactNode } from 'react';
+import { cva } from 'class-variance-authority';
+import type { VariantProps } from 'class-variance-authority';
+
+import { cn } from '@/lib/utils/classname.utils';
+
+// Styles
+// ---------------
+const styles = {
+  header: cva('whitespace-nowrap p-4 text-sm font-bold text-primary-800', {
+    variants: {
+      align: {
+        left: '',
+        right: 'text-right',
+      },
+    },
+  }),
+  cell: cva(
+    [
+      'border-t border-secondary-100 font-light text-xs px-4 flex flex-row items-center lg:text-sm lg:font-normal',
+    ],
+    {
+      variants: {
+        variant: {
+          default: 'text-secondary-600',
+          number: 'tabular-nums text-secondary-600 justify-end',
+          header: 'text-secondary-800',
+        },
+        size: {
+          collapsed: 'py-2.5 md:py-3.5 min-h-[44px] md:min-h-[50px]',
+          relaxed: 'py-3.5',
+        },
+      },
+      defaultVariants: {
+        variant: 'default',
+        size: 'collapsed',
+      },
+    }
+  ),
+};
+
+// Cell
+// ---------------
+export interface CellProps extends VariantProps<typeof styles.cell> {
+  className?: string;
+  children?: React.ReactNode;
+}
+
+export const Cell = ({ variant, size, className, children }: CellProps) => (
+  <div className={cn(styles.cell({ variant, size }), className)}>
+    {children}
+  </div>
+);
+
+// Row
+// ---------------
+interface RowProps {
+  numeration?: ReactNode;
+  className?: string;
+  children?: ReactNode;
+}
+
+const Row = ({ numeration, className, children }: RowProps) => (
+  <div className={cn('grid-cols-subgrid col-span-full grid', className)}>
+    {numeration ? (
+      <Cell key="numeration" className="hidden md:flex">
+        {numeration}
+      </Cell>
+    ) : null}
+    {children}
+  </div>
+);
+
+// Table
+// ---------------
+export interface TableColumnProps {
+  width?: string;
+  align?: 'left' | 'center' | 'right';
+  children?: ReactNode;
+}
+
+export interface TableProps {
+  columns: TableColumnProps[];
+  numeration?: boolean;
+  className?: string;
+  children?: ReactNode;
+}
+
+export const Table = forwardRef<HTMLDivElement, TableProps>(
+  ({ columns, numeration, className, children }, ref) => {
+    const columnWidths = columns.map(({ width }) => width).join(' ');
+    const styles = {
+      '--table-cols': columnWidths,
+      '--md-table-cols': numeration
+        ? `minmax(auto, max-content) ${columnWidths}`
+        : undefined,
+    } as React.CSSProperties;
+
+    /**
+     * align via n-th child on row or table?
+     *
+     * numeration: move it out of the component? maybe we can just hide
+     * the numeration cell without changing the cols var?
+     *
+     * ideally no cloneElement anymore, use context instead
+     */
+
+    return (
+      <div
+        ref={ref}
+        style={styles}
+        className={cn(
+          'grid grid-cols-[var(--table-cols)] overflow-x-auto',
+          numeration && 'md:grid-cols-[var(--md-table-cols)]',
+          className
+        )}
+      >
+        <Row>
+          <Cell>#</Cell>
+          {columns.map(({ children }, idx) => (
+            <Cell key={idx}>{children}</Cell>
+          ))}
+        </Row>
+        {Children.map(children, child => {
+          const row = !isValidElement<{
+            className?: string;
+            variant?: string;
+            size?: string | null;
+          }>(child)
+            ? child
+            : cloneElement(child, {
+                ...child.props,
+              });
+
+          return row;
+        })}
+      </div>
+    );
+  }
+) as Table;
+
+Table.displayName = 'Table';
+Table.Cell = Cell;
+Table.Row = Row;
+
+export interface Table
+  extends React.ForwardRefExoticComponent<
+    TableProps & React.RefAttributes<HTMLTableElement>
+  > {
+  Cell: typeof Cell;
+  Row: typeof Row;
+}
