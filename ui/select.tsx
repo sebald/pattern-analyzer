384c38beed1971e093dbb38ed54a6cce054e35c7
Sebastian Sebald <sebastian.sebald@gmail.com>
2024-01-11 13:50:31 +0100

    nice select

--- ui/select.tsx
+++ ui/select.tsx
@@ -15,32 +15,47 @@ const Option = ({ children, ...props }: SelectOptionProps) => (
 
 // Styles
 // ---------------
-const styles = cva(
-  [
-    'block cursor-pointer rounded-md border-secondary-200 shadow-sm',
-    'hover:border-primary-400',
-    'focus:border-primary-300 focus:ring focus:ring-primary-200 focus:ring-opacity-50',
-    'disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-500',
-  ],
-  {
+const styles = {
+  select: cva(
+    [
+      'block appearance-none bg-white bg-none cursor-pointer rounded-md border-secondary-200 shadow-sm',
+      'hover:border-primary-400',
+      'focus:border-primary-300 focus:ring focus:ring-primary-200 focus:ring-opacity-50',
+      'disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-500',
+    ],
+    {
+      variants: {
+        size: {
+          small: 'py-2 text-xs',
+          regular: 'py-2',
+          large: 'py-3 pl-4 text-xl',
+          huge: 'py-5 pl-4 text-xl',
+        },
+      },
+      defaultVariants: {
+        size: 'regular',
+      },
+    }
+  ),
+  indicator: cva('absolute text-secondary-300', {
     variants: {
       size: {
-        small: 'py-2 text-xs',
-        regular: 'py-2',
-        large: 'py-3 pl-4 text-xl',
-        huge: 'py-5 pl-4 text-xl',
+        small: 'right-2 top-2.5 h-4 w-4',
+        regular: 'right-2 top-3.5 h-4 w-4',
+        large: 'right-2.5 top-[18px] h-5 w-5',
+        huge: 'right-2.5 top-[22px] h-7 w-7',
       },
     },
     defaultVariants: {
       size: 'regular',
     },
-  }
-);
+  }),
+};
 
 // Props
 // ---------------
 export interface SelectProps
-  extends VariantProps<typeof styles>,
+  extends VariantProps<typeof styles.select>,
     Omit<React.ComponentPropsWithRef<'select'>, 'size'> {
   label?: React.ReactNode;
   htmlSize?: React.ComponentPropsWithRef<'select'>['size'];
@@ -60,7 +75,7 @@ export const Select = ({
   const id = useId();
 
   return (
-    <div>
+    <div className="relative">
       {label && (
         <Label htmlFor={id} size={size}>
           {label}
@@ -69,11 +84,23 @@ export const Select = ({
       <select
         {...props}
         id={id}
-        className={styles({ size, className })}
+        className={styles.select({ size, className })}
         size={htmlSize}
       >
         {children}
       </select>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 16 16"
+        fill="currentColor"
+        className={styles.indicator({ size })}
+      >
+        <path
+          fillRule="evenodd"
+          d="M5.22 10.22a.75.75 0 0 1 1.06 0L8 11.94l1.72-1.72a.75.75 0 1 1 1.06 1.06l-2.25 2.25a.75.75 0 0 1-1.06 0l-2.25-2.25a.75.75 0 0 1 0-1.06ZM10.78 5.78a.75.75 0 0 1-1.06 0L8 4.06 6.28 5.78a.75.75 0 0 1-1.06-1.06l2.25-2.25a.75.75 0 0 1 1.06 0l2.25 2.25a.75.75 0 0 1 0 1.06Z"
+          clipRule="evenodd"
+        />
+      </svg>
     </div>
   );
 };
