7ea052dc7a7d80ff5a94bc7fa5609a490824f524
Sebastian Sebald <sebastian.sebald@gmail.com>
2024-01-16 16:28:04 +0100

    improve? imprint

--- app/_components/site-footer.tsx
+++ app/_components/site-footer.tsx
@@ -5,7 +5,7 @@ export const SiteFooter = () => (
   <footer className="border border-b border-primary-200 px-4 py-6">
     <Container className="flex flex-col items-center justify-between gap-8 md:flex-row">
       <p className="text-sm text-secondary-600">
-        This is an unofficial website and is not affiliated with Atomic Mass
+        This is an unofficial website. It is not affiliated with Atomic Mass
         Games, Lucasfilm Ltd., or Disney.
       </p>
       <a
